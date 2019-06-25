fa8fc32e474f9a5f6b05ee8011b5274425f031d3
Volodymyr Hrushovenko <volkulion@gmail.com>
2019-06-26 02:31:07 +0300

    test

--- terraform/workspaces.tf
+++ terraform/workspaces.tf
@@ -1,10 +1,11 @@
-/* terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "smartit"
+terraform {
+  backend "azurerm" {
+    storage_account_name = "terraformvolkulion"
+    container_name       = "terraform"
+    key                  = "dev.terraform.tera"
 
-    workspaces {
-      name = "lab10"
-    }
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "07fdWOaYDuW4t0tzKzjDA2kGtc7cFAV0xcgPgM3nu/peq8L96Lr9p//63txkQbeGZlDgYvf+HOf+Sq0QMkhbXg==/2+xGJw7Oe6f+rC2B4oqXg=="
   }
-} */
+}
