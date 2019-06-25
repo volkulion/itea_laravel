6c9e99eaa4016cf8d65e26691505479b358c5800
Volodymyr Hrushovenko <volkulion@gmail.com>
2019-06-26 02:39:44 +0300

    test

--- terraform/workspaces.tf
+++ terraform/workspaces.tf
@@ -6,6 +6,6 @@ terraform {
 
     # rather than defining this inline, the Access Key can also be sourced
     # from an Environment Variable - more information is available below.
-    access_key = "07fdWOaYDuW4t0tzKzjDA2kGtc7cFAV0xcgPgM3nu/peq8L96Lr9p//63txkQbeGZlDgYvf+HOf+Sq0QMkhbXg==/2+xGJw7Oe6f+rC2B4oqXg=="
+    access_key = "07fdWOaYDuW4t0tzKzjDA2kGtc7cFAV0xcgPgM3nu/peq8L96Lr9p//63txkQbeGZlDgYvf+HOf+Sq0QMkhbXg=="
   }
 }
